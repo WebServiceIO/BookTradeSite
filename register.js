b0a53ef0be68af639f97d303175aa37b649b73f8
David Ghermezi <dghermezi@cpp.edu>
2016-10-05 11:29:44 -0700

    checks for cpp.edu email

--- register.js
+++ register.js
@@ -1,27 +1,28 @@
 function validateForm() {
-    var x = document.forms["register"]["email"].value;
-    if (x == null || x == "") {
-        alert("E-mail must not be empty.");
-        return false;
-      }
-      x = document.forms["register"]["username"].value;
-      if (x == null || x == "") {
-          alert("User name must not be empty.");
-          return false;
-    }
-      x = document.forms["register"]["password"].value;
-      if (x == null || x == "") {
-          alert("Password must not be empty.");
-          return false;
-    }
-    x = document.forms["register"]["first_name"].value;
-    if (x == null || x == "") {
-        alert("First name must not be empty.");
-        return false;
-  }
-  x = document.forms["register"]["last_name"].value;
-  if (x == null || x == "") {
-      alert("Last name must not be empty.");
+   var patt = new RegExp("@cpp.edu");
+   var x = document.forms["register"]["email"].value;
+   if (x == null || x == "" || patt.test(x) == false) {
+      alert("Invalid E-mail!");
       return false;
-}
+   }
+   x = document.forms["register"]["username"].value;
+   if (x == null || x == "") {
+      alert("User name must not be empty.");
+      return false;
+   }
+   x = document.forms["register"]["password"].value;
+   if (x == null || x == "") {
+      alert("Password must not be empty.");
+      return false;
+   }
+   x = document.forms["register"]["first_name"].value;
+   if (x == null || x == "") {
+      alert("First name must not be empty.");
+      return false;
+   }
+   x = document.forms["register"]["last_name"].value;
+   if (x == null || x == "") {
+   alert("Last name must not be empty.");
+   return false;
+   }
 }
