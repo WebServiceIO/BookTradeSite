2def4580fd3605daf41cef6a13ccabaa9c1f39a2
Tanner Summers <Tannersummers@gmail.com>
2016-11-01 14:58:49 -0700

    - Datatables ajax fixes

--- userAccount.php
+++ userAccount.php
@@ -8,8 +8,8 @@
 
     <!--- Bootstrap CDN -->
     <link rel = "stylesheet" href = "http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-<!--   <script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>-->
-    <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+   <script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<!--    <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>-->
     <script src = "http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 
     <!-- CSS -->
