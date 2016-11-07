b0c743f70a7e3d1f44b4db019472c15be37b8bd1
Christine <christine.tn.nguyen@gmail.com>
2016-11-07 15:08:11 -0800

    Fix login and register pages

--- login.php
+++ login.php
@@ -27,7 +27,7 @@
             require_once('includes/php/security.php');
             require_once('includes/php/db_util.php');
             require_once('includes/php/session.php');
-            require_once ('includes/php/config.php');
+            require_once('includes/php/config/config.php');
 
             // start a session for login
             session_start();
