0aad635b4be26d58e5bc457c7901d561f859175f
Tanner Summers <Tannersummers@gmail.com>
2016-11-25 13:32:11 -0800

    - Change from ssl to standard email settings

--- includes/php/emailServerConnection.php
+++ includes/php/emailServerConnection.php
@@ -4,16 +4,20 @@
 class emailServerConnection{
 
     static function connectToEmail(){
-        $servername = 'ssl://smtp.gmail.com';
-        //$servername = 'smtp.gmail.com';
+        //$servername = 'ssl://smtp.gmail.com';
+        $servername = 'smtp.gmail.com';
         $username = 'bkxchnge@gmail.com';
         $password = 'cdgt1234';
-        //$port = 25;
+        $port = 25;
         //$port = 587;
-        $port = 465;
+        //$port = 465;
 
 
-        $connection = Swift_SmtpTransport::newInstance($servername, $port, "ssl")
+//        $connection = Swift_SmtpTransport::newInstance($servername, $port, "ssl")
+//            ->setUsername($username)
+//            ->setPassword($password);
+
+        $connection = Swift_SmtpTransport::newInstance($servername, $port)
             ->setUsername($username)
             ->setPassword($password);
 
