89f535a69f37146a42107fd209805c98a4f90006
TannerS <tannersummers@gmail.com>
2016-11-26 13:43:39 -0800

    - Updated login file reading

--- includes/php/config/db_injection.php
+++ includes/php/config/db_injection.php
@@ -1,73 +1,74 @@
 <?php
 require_once 'config.php';
 
-// *****************DEBUGGG
-
-
-
-$user = "";
-$x = "";
-
-$handle = fopen("/var/admin/admin", "r");
-
-if ($handle)
-{
-    //while (($line = fgets($handle)) !== false) {
-    if(($cur = fgets($handle)) !== false)
-    {
-        $user = $cur;
-    }
-    else
-        echo 'ERROR READING FILE';
-
-    if(($cur = fgets($handle)) !== false)
-    {
-        $x = $cur;
-    }
-    else
-        echo 'ERROR READING FILE';
-
-    fclose($handle);
-} else {
-    echo 'ERROR OPENING FILE';
-}
-
-
-echo 'USER DEBUG' . $user;
-echo 'DEBUG' . $x;
-
-
-
-// *****************DEBUGGG
-
-
-
 class DataBaseLoader
 {
-
     protected $db_connection;
-
-
-
 
     static function connect() {
 
         $db_connection = null;
 
+        $user = "";
+        $pass = "";
+        $db = "";
+        $host = "";
 
         try {
-            $db_connection = new PDO('mysql:dbname=' . db . ';host=' . db_host, db_login, db_pass);
-            // set the PDO error mode to exception
-            $db_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            //echo "Connected successfully";
+
+            $handle = fopen("/var/admin/admin", "r");
+
+            if ($handle)
+            {
+                //while (($line = fgets($handle)) !== false) {
+                if(($cur = fgets($handle)) !== false)
+                {
+                    $user = $cur;
+                }
+                else
+                    echo 'Error has occurred, please report to admin';
+
+                if(($cur = fgets($handle)) !== false)
+                {
+                    $x = $cur;
+                }
+                else
+                    echo 'Error has occurred, please report to admin';
+
+                if(($cur = fgets($handle)) !== false)
+                {
+                    $db = $cur;
+                }
+                else
+                    echo 'Error has occurred, please report to admin';
+
+                if(($cur = fgets($handle)) !== false)
+                {
+                    $host = $cur;
+                }
+                else
+                    echo 'Error has occurred, please report to admin';
+
+                fclose($handle);
+            } else {
+                echo 'Critical error has occurred, please report to admin';
+            }
+
+                $db_connection = new PDO('mysql:dbname=' . db . ';host=' . db_host, db_login, db_pass);
+                // set the PDO error mode to exception
+                $db_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                //echo "Connected successfully";
         }
         catch(PDOException $e)
         {
-            echo "Connection failed: " . $e->getMessage();
+            echo $e->getMessage();
+        }
+        catch ( Exception $e ) {
+            echo $e->getMessage();
         }
 
 
-        return $db_connection;
+return $db_connection;
     }
 }
 ?>
