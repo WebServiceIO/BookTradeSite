e3100641a8bfd437c94d5dcf7769babe41e12f31
TannerS <tannersummers@gmail.com>
2016-11-26 14:01:55 -0800

    - Undo last debug

--- includes/php/config/db_injection.php
+++ includes/php/config/db_injection.php
@@ -16,43 +16,43 @@ class DataBaseLoader
 
         try {
 
-//            $handle = fopen("/var/admin/admin", "r");
-//
-//            if ($handle)
-//            {
-//                //while (($line = fgets($handle)) !== false) {
-//                if(($cur = fgets($handle)) !== false)
-//                {
-//                    $user = $cur;
-//                }
-//                else
-//                    echo 'Error has occurred, please report to admin';
-//
-//                if(($cur = fgets($handle)) !== false)
-//                {
-//                    $x = $cur;
-//                }
-//                else
-//                    echo 'Error has occurred, please report to admin';
-//
-//                if(($cur = fgets($handle)) !== false)
-//                {
-//                    $db = $cur;
-//                }
-//                else
-//                    echo 'Error has occurred, please report to admin';
-//
-//                if(($cur = fgets($handle)) !== false)
-//                {
-//                    $host = $cur;
-//                }
-//                else
-//                    echo 'Error has occurred, please report to admin';
-//
-//                fclose($handle);
-//            } else {
-//                echo 'Critical error has occurred, please report to admin';
-//            }
+            $handle = fopen("/var/admin/admin", "r");
+
+            if ($handle)
+            {
+                //while (($line = fgets($handle)) !== false) {
+                if(($cur = fgets($handle)) !== false)
+                {
+                    $user = $cur;
+                }
+                else
+                    echo 'Error has occurred, please report to admin';
+
+                if(($cur = fgets($handle)) !== false)
+                {
+                    $x = $cur;
+                }
+                else
+                    echo 'Error has occurred, please report to admin';
+
+                if(($cur = fgets($handle)) !== false)
+                {
+                    $db = $cur;
+                }
+                else
+                    echo 'Error has occurred, please report to admin';
+
+                if(($cur = fgets($handle)) !== false)
+                {
+                    $host = $cur;
+                }
+                else
+                    echo 'Error has occurred, please report to admin';
+
+                fclose($handle);
+            } else {
+                echo 'Critical error has occurred, please report to admin';
+            }
 
                 $db_connection = new PDO('mysql:dbname=' . $db . ';host=' . $host, $user, $pass);
                 // set the PDO error mode to exception
