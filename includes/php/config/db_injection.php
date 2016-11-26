540693e46f6985d3714a62b07b9f98a51878a0bf
Tanner Summers <Tannersummers@gmail.com>
2016-11-26 13:24:20 -0800

    - Testing file reading login

--- includes/php/config/db_injection.php
+++ includes/php/config/db_injection.php
@@ -1,43 +1,59 @@
 <?php
 require_once 'config.php';
+
+// *****************DEBUGGG
+
+
+
+$user = "";
+$x = "";
+
+$handle = fopen("var/admin/admin", "r");
+
+if ($handle)
+{
+    //while (($line = fgets($handle)) !== false) {
+    if(($cur = fgets($handle)) !== false)
+    {
+        $user = $cur;
+    }
+    else
+        echo 'ERROR READING FILE';
+
+    if(($cur = fgets($handle)) !== false)
+    {
+        $x = $cur;
+    }
+    else
+        echo 'ERROR READING FILE';
+
+    fclose($handle);
+} else {
+    echo 'ERROR OPENING FILE';
+}
+
+
+echo 'USER DEBUG' . $user;
+echo 'DEBUG' . $x;
+
+
+
+// *****************DEBUGGG
+
+
+
 class DataBaseLoader
 {
 
     protected $db_connection;
 
+
+
+
     static function connect() {
 
         $db_connection = null;
-        $user = "";
-        $x = "";
 
-        $handle = fopen("var/admin/admin", "r");
-
-        if ($handle)
-        {
-            //while (($line = fgets($handle)) !== false) {
-            if(($cur = fgets($handle)) !== false)
-            {
-                $user = $cur;
-            }
-            else
-                echo 'ERROR READING FILE';
-
-            if(($cur = fgets($handle)) !== false)
-            {
-                $x = $cur;
-            }
-            else
-                echo 'ERROR READING FILE';
-
-            fclose($handle);
-        } else {
-            echo 'ERROR OPENING FILE';
-        }
-
-
-        echo 'USER DEBUG' . $user;
-        echo 'DEBUG' . $x;
 
         try {
             $db_connection = new PDO('mysql:dbname=' . db . ';host=' . db_host, db_login, db_pass);
