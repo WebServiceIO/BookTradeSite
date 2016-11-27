18375364efa7b3e534c8e5806f893902f3ef003e
Christine <christine.tn.nguyen@gmail.com>
2016-11-26 17:47:11 -0800

    Delete ok table in results

--- includes/js/tables/results_post_data_tables.js
+++ includes/js/tables/results_post_data_tables.js
@@ -123,65 +123,6 @@ $(document).ready(function() {
 
 
 
-    $("#condition_ok_datatable").dataTable({
-        aLengthMenu: [[5, 10, 25, 50, 100, 500, 1000], [5, 10, 25, 50, 100, 500, 1000]],
-        iDisplayLength: 5,
-        sPaginationType: "full_numbers",
-        bProcessing: true,
-        oLanguage: {
-            sProcessing: "Retrieving all post...",
-            sLengthMenu: "Showing _MENU_ entries &nbsp;",
-            sInfo: "(_START_ to _END_ of _TOTAL_ total)",
-            oPaginate: {
-                sPrevious: "<",
-                sNext: ">",
-                sFirst: "<<",
-                sLast: ">>"
-            }
-        },
-        columnDefs: [
-            {
-                "aTargets" : [7],
-                "data": null,
-                "mData": function (source, type, val)
-                {
-                    var post_id = source[0];
-                    return  '<form name="post" action ="' + window.location.pathname.replace(/[^\\\/]*$/, '') + 'post_info.php" method="post">  ' +
-                        '<input type="hidden" name="post_id" value="' + post_id + '"/> ' +
-                        '<button type="submit" class="btn btn-default btn-transparent">View</button> ' +
-                        '</form>';
-                }
-            }
-        ],
-        aoColumns:[
-            {"title": "Post"},
-            {"title": "Title"},
-            {"title": "Author"},
-            {"title": "Edition"},
-            {"title": "Class"},
-            {"title": "Price"},
-            {"title": "Comments"},
-            {"title": "More Info"}
-        ],
-        bJQueryUI: false,
-        sDom: '<"H"fli><"proc1"r>t<"proc2"r><"F"pli>',
-        aaSorting: [[0, "asc"]],
-        bServerSide: true,
-        sAjaxSource: "includes/php/tables/serverside_processing_condition_ok.php",
-        bDeferRender: true
-    });
-
-
-
-
-
-
-
-
-
-
-
-
     $("#condition_acceptable_datatable").dataTable({
         aLengthMenu: [[5, 10, 25, 50, 100, 500, 1000], [5, 10, 25, 50, 100, 500, 1000]],
         iDisplayLength: 5,
